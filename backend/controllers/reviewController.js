b1d09ec1348c22dc411367d309af0f064bfb1178
PaulObuba <paulobuba26@gmail.com>
2023-07-07 16:55:06 +0100

    last commit

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,21 +1,22 @@
-import Reviews from '../models/Review.js';
-import Tour from '../models/Tour.js';
+import Reviews from "../models/Review.js";
+import Tour from "../models/Tour.js";
 
 export const createReviews = async (req, res) => {
-
   const tourId = req.params.tourId;
-  const newReviews = new Reviews({ ...req.body })
+  const newReviews = new Reviews({ ...req.body });
 
   try {
     const savedReview = await newReviews.save();
 
     // after creating a new review now update the reviews array of the tour
     await Tour.findByIdAndUpdate(tourId, {
-      $push: { reviews: savedReview._id }
-    })
+      $push: { reviews: savedReview._id },
+    });
 
-    res.status(200).json({ success: true, message: "Review submitted" })
+    res.status(200).json({ success: true, message: "Review submitted" });
   } catch (err) {
-    res.status(500).json({ success: false, message: `${err}   ...Faild to submit` })
+    res
+      .status(500)
+      .json({ success: false, message: `${err}   ...Faild to submit` });
   }
-}
+};
